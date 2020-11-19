2989f1d3661fefe3dcbc294bbcec9f5d8c163842
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-19 21:23:25 +0100

    Iteration 2: The Character Class

--- main.js
+++ main.js
@@ -30,3 +30,21 @@ function drawEverything() {
 }
 
 drawEverything();
+
+class Charachter {
+  constructor(col, row) {
+    (this.col = col), (this.row = row);
+  }
+  moveUp() {
+    this.row -= 1;
+  }
+  moveRight() {
+    this.col += 1;
+  }
+  moveDown() {
+    this.row += 1;
+  }
+  moveLeft() {
+    this.col -= 1;
+  }
+}
