64e3039ae7efaaebef29e696b4e36a4e35f719b7
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-19 21:14:43 +0100

    Iteration 1: Drawing the Grid

---
+++ main.js
@@ -0,0 +1,32 @@
+const canvasElement = document.querySelector('canvas');
+const context = canvasElement.getContext('2d');
+
+const w = canvasElement.width;
+const h = canvasElement.height;
+
+// Iteration 1
+function drawGrid(w, h) {
+  // TODO: write the code of the function
+  //   context.canvasElement.width = w;
+  //   context.canvasElement.height = h;
+  for (x = 0; x <= w; x += 50) {
+    for (y = 0; y <= h; y += 50) {
+      context.moveTo(x, 0);
+      context.lineTo(x, h);
+      context.stroke();
+      context.moveTo(0, y);
+      context.lineTo(w, y);
+      context.stroke();
+    }
+  }
+}
+
+drawGrid(800, 400);
+
+function drawEverything() {
+  drawGrid();
+  // drawPlayer()
+  // drawTreasure()
+}
+
+drawEverything();
