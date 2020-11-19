3f6ce776dcb886a45365a8e5f2444c9f55fdc2b0
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-19 21:50:20 +0100

    Iteration 4: Unfinished

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ function drawGrid(w, h) {
 drawGrid(800, 400);
 
 drawEverything();
+
 // Iteration 2: The Character Class
+
 class Charachter {
   constructor(col, row) {
     (this.col = col), (this.row = row);
@@ -41,6 +43,7 @@ class Charachter {
 }
 
 // Iteration 3: Drawing the Player
+
 function drawPlayer() {
   const player = new Image();
   player.src = 'images/character-down.png';
@@ -52,8 +55,33 @@ function drawPlayer() {
   });
 }
 
+//Iteration 4: The Treasure Class
+
+class Treasure {
+  constructor() {}
+
+  setRandomPosition(col, row) {
+    // col = Math.floor(Math.random() * 500);
+    // row = Math.floor(Math.random() * 500);
+  }
+}
+
+function drawTreasure() {
+  const treasure = new Image();
+  treasure.src = 'images/treasure.png';
+
+  treasure.addEventListener('load', () => {
+    // treasure.setRandomPosition(col, row);
+    const width = 45;
+    const ratio = treasure.height / treasure.width;
+    context.drawImage(treasure, 50, 50, width, width * ratio);
+  });
+}
+// Create a class Treasure with a method setRandomPosition() and a property col and row
+// Create a function drawTreasure() that displays the treasure on the canvas. The picture is images/treasure.png
+
 function drawEverything() {
   drawGrid();
   drawPlayer();
-  // drawTreasure()
+  drawTreasure();
 }
