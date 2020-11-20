715c5698dbd8c07eefd2d34a2187943e493354c0
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-20 23:19:41 +0100

    Iteration 4 & 5 with the help of the solution

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ const context = canvasElement.getContext('2d');
 
 const w = canvasElement.width;
 const h = canvasElement.height;
+const tileCount = 10;
+const tileSize = w / tileCount;
 
 // Iteration 1
-function drawGrid(w, h) {
-  for (x = 0; x <= w; x += 50) {
-    for (y = 0; y <= h; y += 50) {
+function drawGrid() {
+  context.lineWidth = 1;
+  for (x = 0; x <= h; x += tileSize) {
+    for (y = 0; y <= w; y += tileSize) {
+      context.beginPath();
       context.moveTo(x, 0);
       context.lineTo(x, h);
       context.stroke();
+      context.beginPath();
       context.moveTo(0, y);
       context.lineTo(w, y);
       context.stroke();
@@ -18,70 +23,125 @@ function drawGrid(w, h) {
   }
 }
 
-drawGrid(800, 400);
-
-drawEverything();
-
 // Iteration 2: The Character Class
 
-class Charachter {
-  constructor(col, row) {
-    (this.col = col), (this.row = row);
+class Character {
+  constructor(initialCol, initalRow) {
+    (this.col = initialCol),
+      (this.row = initalRow),
+      (this.direction = 'down'),
+      (this.score = 0);
+
+    const imagePaths = {
+      left: 'images/character-left.png',
+      up: 'images/character-up.png',
+      right: 'images/character-right.png',
+      down: 'images/character-down.png'
+    };
+
+    this.images = {};
+
+    for (let direction in imagePaths) {
+      this.images[direction] = new Image();
+      this.images[direction].src = imagePaths[direction];
+    }
   }
+
   moveUp() {
-    this.row -= 1;
-  }
-  moveRight() {
-    this.col += 1;
+    this.row--;
+    this.direction = 'up';
   }
   moveDown() {
-    this.row += 1;
+    this.row++;
+    this.direction = 'down';
   }
+  moveRight() {
+    this.col++;
+    this.direction = 'right';
+  }
+
   moveLeft() {
-    this.col -= 1;
+    this.col--;
+    this.direction = 'left';
   }
 }
-
+const player = new Character(0, 0);
 // Iteration 3: Drawing the Player
 
 function drawPlayer() {
-  const player = new Image();
-  player.src = 'images/character-down.png';
-
-  player.addEventListener('load', () => {
-    const width = 45;
-    const ratio = player.height / player.width;
-    context.drawImage(player, 0, 0, width, width * ratio);
-  });
+  context.drawImage(
+    player.images[player.direction],
+    player.col * tileSize,
+    player.row * tileSize,
+    tileSize,
+    tileSize
+  );
 }
 
 //Iteration 4: The Treasure Class
 
 class Treasure {
-  constructor() {}
+  constructor() {
+    this.setRandomPosition();
 
-  setRandomPosition(col, row) {
-    // col = Math.floor(Math.random() * 500);
-    // row = Math.floor(Math.random() * 500);
+    this.image = new Image();
+    this.image.src = 'images/treasure.png';
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * tileCount);
+    this.row = Math.floor(Math.random() * tileCount);
   }
 }
+const treasure = new Treasure();
 
 function drawTreasure() {
-  const treasure = new Image();
-  treasure.src = 'images/treasure.png';
-
-  treasure.addEventListener('load', () => {
-    // treasure.setRandomPosition(col, row);
-    const width = 45;
-    const ratio = treasure.height / treasure.width;
-    context.drawImage(treasure, 50, 50, width, width * ratio);
-  });
+  context.drawImage(
+    treasure.image,
+    treasure.col * tileSize,
+    treasure.row * tileSize,
+    tileSize,
+    tileSize
+  );
 }
-// Create a class Treasure with a method setRandomPosition() and a property col and row
-// Create a function drawTreasure() that displays the treasure on the canvas. The picture is images/treasure.png
+
+document.addEventListener('keydown', (event) => {
+  // Stop the default behavior (moving the screen to the left/up/right/down)
+  event.preventDefault();
+
+  const key = event.key;
+  switch (key) {
+    case 'ArrowLeft':
+      player.moveLeft();
+      break;
+    case 'ArrowUp':
+      player.moveUp();
+      break;
+    case 'ArrowRight':
+      player.moveRight();
+      break;
+    case 'ArrowDown':
+      player.moveDown();
+      break;
+  }
+
+  if (player.row === treasure.row && player.col === treasure.col) {
+    player.score++;
+    const newElement = document.createElement('span');
+    newElement.innerText = ` ${player.score}`;
+    const scoreId = document.getElementById('score');
+    scoreId.appendChild(newElement);
+    treasure.setRandomPosition();
+  }
+  drawEverything();
+});
 
 function drawEverything() {
+  context.clearRect(0, 0, w, h);
   drawGrid();
   drawPlayer();
   drawTreasure();
 }
+
+// Add score to html
+setTimeout(drawEverything, 500);
